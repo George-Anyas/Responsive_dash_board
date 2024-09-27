e2117e8d3034ff205179ab1aefa0398ab06a55f0
GeorgeSoliman <georgelove462@gmail.com>
2024-09-27 17:29:56 +0300

    create file custom drawwer & create user info list tile

--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -1,49 +1,51 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-  
-  /// Assets for imagesCardReceive
+
+  /// Assets for assetsImagesCardReceive
   /// assets/images/card-receive.svg
-  static const String imagesCardReceive = "assets/images/card-receive.svg";
+  static const String assetsImagesCardReceive =
+      "assets/images/card-receive.svg";
 
-  /// Assets for imagesCardSend
+  /// Assets for assetsImagesCardSend
   /// assets/images/card-send.svg
-  static const String imagesCardSend = "assets/images/card-send.svg";
+  static const String assetsImagesCardSend = "assets/images/card-send.svg";
 
-  /// Assets for imagesDashboard
+  /// Assets for assetsImagesDashboard
   /// assets/images/dashboard.svg
-  static const String imagesDashboard = "assets/images/dashboard.svg";
+  static const String assetsImagesDashboard = "assets/images/dashboard.svg";
 
-  /// Assets for imagesFrame
+  /// Assets for assetsImagesFrame
   /// assets/images/Frame.svg
-  static const String imagesFrame = "assets/images/Frame.svg";
+  static const String assetsImagesFrame = "assets/images/Frame.svg";
 
-  /// Assets for imagesLogout
+  /// Assets for assetsImagesLogout
   /// assets/images/logout.svg
-  static const String imagesLogout = "assets/images/logout.svg";
+  static const String assetsImagesLogout = "assets/images/logout.svg";
 
-  /// Assets for imagesMaskgroup
-  /// assets/images/Maskgroup.svg
-  static const String imagesMaskgroup = "assets/images/Maskgroup.svg";
+  /// Assets for assetsImagesMask group
+  /// assets/images/Mask group.svg
+  static const String assetsImagesMaskgroup = "assets/images/Mask group.svg";
 
-  /// Assets for imagesMyInvestments
+  /// Assets for assetsImagesMyInvestments
   /// assets/images/my_investments.svg
-  static const String imagesMyInvestments = "assets/images/my_investments.svg";
+  static const String assetsImagesMyInvestments =
+      "assets/images/my_investments.svg";
 
-  /// Assets for imagesMyTransaction
+  /// Assets for assetsImagesMyTransaction
   /// assets/images/my_transaction.svg
-  static const String imagesMyTransaction = "assets/images/my_transaction.svg";
+  static const String assetsImagesMyTransaction =
+      "assets/images/my_transaction.svg";
 
-  /// Assets for imagesSetting22x
+  /// Assets for assetsImagesSetting22x
   /// assets/images/setting-2@2x.svg
-  static const String imagesSetting22x = "assets/images/setting-2@2x.svg";
+  static const String assetsImagesSetting22x = "assets/images/setting-2@2x.svg";
 
-  /// Assets for imagesStatics
+  /// Assets for assetsImagesStatics
   /// assets/images/statics.svg
-  static const String imagesStatics = "assets/images/statics.svg";
+  static const String assetsImagesStatics = "assets/images/statics.svg";
 
-  /// Assets for imagesWallet2
+  /// Assets for assetsImagesWallet2
   /// assets/images/wallet-2.svg
-  static const String imagesWallet2 = "assets/images/wallet-2.svg";
+  static const String assetsImagesWallet2 = "assets/images/wallet-2.svg";
 }
-
