ed91e66a89c72a29ab273faa5cfe0821465ca628
GeorgeSoliman <georgelove462@gmail.com>
2024-09-28 16:27:24 +0300

    preview  list tile  & fix issue

--- lib/widget/custom_drawer.dart
+++ lib/widget/custom_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_bord/utils/app_images.dart';
+import 'package:responsive_dash_bord/widget/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
@@ -7,7 +9,12 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: const Column(
-        children: [],
+        children: [
+          UserInfoListTile(
+              image: Assets.assetsImagesFrame,
+              title: 'Lekan Okeowo',
+              subtitle: 'demo@gmail.com')
+        ],
       ),
     );
   }
--- lib/widget/user_info_list_tile.dart
+++ lib/widget/user_info_list_tile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:flutter_svg/svg.dart';
-import 'package:responsive_dash_bord/utils/app_images.dart';
+
 import 'package:responsive_dash_bord/utils/app_style.dart';
 
 class UserInfoListTile extends StatelessWidget {
@@ -13,15 +13,19 @@ class UserInfoListTile extends StatelessWidget {
   final String image, title, subtitle;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(image),
-      title: Text(
-        title,
-        style: AppStyles.styleSemiBold16(context),
-      ),
-      subtitle: Text(
-        subtitle,
-        style: AppStyles.styleRegular12(context),
+    return Card(
+      elevation: 0,
+      color: Color(0xffFAFAFA),
+      child: ListTile(
+        leading: SvgPicture.asset(image),
+        title: Text(
+          title,
+          style: AppStyles.styleSemiBold16(context),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: AppStyles.styleRegular12(context),
+        ),
       ),
     );
   }
