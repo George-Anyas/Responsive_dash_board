9d413da32fb55d8c40ae98d9d3dfd74dd7bcba5b
GeorgeSoliman <georgelove462@gmail.com>
2024-10-01 16:06:33 +0300

    create all expensess header part two

--- lib/widget/all_expensess_header.dart
+++ lib/widget/all_expensess_header.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_bord/utils/app_style.dart';
+import 'package:responsive_dash_bord/widget/range_options.dart';
 
 class AllExpensessHeader extends StatelessWidget {
   const AllExpensessHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [],
+    return Row(
+      children: [
+        Text(
+          'All Expenses',
+          style: AppStyles.styleSemiBold20(context),
+        ),
+        const Expanded(
+          child: SizedBox(),
+        ),
+        const RangeOptions()
+      ],
     );
   }
 }
---
+++ lib/widget/range_options.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_bord/utils/app_style.dart';
+
+class RangeOptions extends StatelessWidget {
+  const RangeOptions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(
+            width: 1,
+            color: Color(0xff1f1f1),
+          ),
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+      child: Row(
+        children: [
+          Text(
+            'Monthly',
+            style: AppStyles.styleMedium16(context),
+          ),
+          const SizedBox(
+            width: 18,
+          ),
+          Transform.rotate(
+              angle: -1.57079633,
+              child: const Icon(
+                Icons.arrow_back_ios_new_outlined,
+                color: Color(0xFF064061),
+              ))
+        ],
+      ),
+    );
+  }
+}
