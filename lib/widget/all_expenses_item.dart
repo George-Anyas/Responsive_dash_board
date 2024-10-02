cbbdd85c6b743cef17fe297978a722c9babedf56
GeorgeSoliman <georgelove462@gmail.com>
2024-10-02 17:36:47 +0300

    all expenses item part 3

--- lib/widget/all_expenses_item.dart
+++ lib/widget/all_expenses_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_bord/models/all_expenses_item_model.dart';
+import 'package:responsive_dash_bord/utils/app_style.dart';
 import 'package:responsive_dash_bord/widget/all_expenses_item_header.dart';
 
 class AllExpensesItem extends StatelessWidget {
@@ -19,9 +20,31 @@ class AllExpensesItem extends StatelessWidget {
           ),
           color: Colors.white),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AllExpensesItemHeader(
             image: itemModel.image,
+          ),
+          const SizedBox(
+            height: 34,
+          ),
+          Text(
+            itemModel.title,
+            style: AppStyles.styleMedium16(context),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            itemModel.date,
+            style: AppStyles.styleRegular14(context),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            itemModel.price,
+            style: AppStyles.styleSemiBold24(context),
           )
         ],
       ),
