5d0f7a70ded91f7776b7d638b2aadae6bb8eda4f
GeorgeSoliman <georgelove462@gmail.com>
2024-10-03 17:26:32 +0300

    create custom back groung container

--- lib/widget/all_expensess.dart
+++ lib/widget/all_expensess.dart
@@ -2,19 +2,15 @@ import 'package:flutter/material.dart';
 
 import 'package:responsive_dash_bord/widget/all_expenses_item_listView.dart';
 import 'package:responsive_dash_bord/widget/all_expensess_header.dart';
+import 'package:responsive_dash_bord/widget/custom_background_container.dart';
 
 class AllExpensess extends StatelessWidget {
   const AllExpensess({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-          color: Colors.white,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16))),
-      child: const Column(
+    return const CustomBackgroundContainer(
+      child: Column(
         children: [
           AllExpensessHeader(),
           AllExpensesItemListview(),
