534ff13bd99dfa00ee3e082f768e631a44cab740
GeorgeSoliman <georgelove462@gmail.com>
2024-10-03 15:32:29 +0300

    all expenses section

--- lib/widget/all_expenses_item_listView.dart
+++ lib/widget/all_expenses_item_listView.dart
@@ -3,7 +3,7 @@ import 'package:responsive_dash_bord/models/all_expenses_item_model.dart';
 import 'package:responsive_dash_bord/utils/app_images.dart';
 import 'package:responsive_dash_bord/widget/all_expenses_item.dart';
 
-class AllExpensesItemListview extends StatelessWidget {
+class AllExpensesItemListview extends StatefulWidget {
   const AllExpensesItemListview({super.key});
   static const items = [
     AllExpensesItemModel(
@@ -22,27 +22,51 @@ class AllExpensesItemListview extends StatelessWidget {
         date: 'April 2022',
         price: r'$20,129')
   ];
+
+  @override
+  State<AllExpensesItemListview> createState() =>
+      _AllExpensesItemListviewState();
+}
+
+class _AllExpensesItemListviewState extends State<AllExpensesItemListview> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: items.asMap().entries.map((e) {
+      children: AllExpensesItemListview.items.asMap().entries.map((e) {
         int index = e.key;
         var item = e.value;
         if (index == 1) {
           return Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: AllExpensesItem(isSelected: false, itemModel: item),
+            child: GestureDetector(
+              onTap: () {
+                updateIndex(index);
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: AllExpensesItem(
+                    isSelected: selectedIndex == index, itemModel: item),
+              ),
             ),
           );
         } else {
           return Expanded(
-              child: AllExpensesItem(isSelected: false, itemModel: item));
+            child: GestureDetector(
+              onTap: () {
+                updateIndex(index);
+              },
+              child: AllExpensesItem(
+                  isSelected: selectedIndex == index, itemModel: item),
+            ),
+          );
         }
       }).toList(),
     );
-    // children: items
-    //     .map((e) => Expanded(child: AllExpensesItem(itemModel: e)))
-    //     .toList());
+  }
+
+  void updateIndex(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
   }
 }
