c5772a89213fcef4eb5dd723c2849af2be95a99b
GeorgeSoliman <georgelove462@gmail.com>
2024-10-08 12:39:30 +0300

    create my card

--- lib/widget/dashboard_desktop_layout.dart
+++ lib/widget/dashboard_desktop_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:responsive_dash_bord/widget/all_expensess_and_quick_invoice_section.dart';
 import 'package:responsive_dash_bord/widget/custom_drawer_item.dart';
+import 'package:responsive_dash_bord/widget/my_card.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -22,6 +23,7 @@ class DashboardDesktopLayout extends StatelessWidget {
             child: AllExpensessAndQuickInvoiceSection(),
           ),
         ),
+        Expanded(child: MyCard())
       ],
     );
   }
