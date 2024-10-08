f4eb5f92c15ed32234fac345d8fbb21d784a3941
GeorgeSoliman <georgelove462@gmail.com>
2024-10-08 16:05:57 +0300

    my card done

--- lib/widget/my_card.dart
+++ lib/widget/my_card.dart
@@ -13,26 +13,58 @@ class MyCard extends StatelessWidget {
       aspectRatio: 420 / 215,
       child: Container(
         decoration: ShapeDecoration(
-            image: const DecorationImage(
-              image: AssetImage(
-                Assets.imagesMaskgroup,
+          image: const DecorationImage(
+            image: AssetImage(
+              Assets.imagesMaskgroup,
+            ),
+          ),
+          color: const Color(0XFF4EB7F1),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              12,
+            ),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding:
+                  const EdgeInsets.only(top: 16, left: 30, right: 40),
+              title: Text(
+                'My Name',
+                style: AppStyles.styleRegular16(context)
+                    .copyWith(color: Colors.white),
+              ),
+              subtitle: Text(
+                'George Soliman',
+                style: AppStyles.styleMedium20(context),
+              ),
+              trailing: SvgPicture.asset(Assets.imagesGallery),
+            ),
+            const Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.only(right: 48 - 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    '0918 8124 0042 8129',
+                    style: AppStyles.styleSemiBold24(context)
+                        .copyWith(color: Colors.white),
+                  ),
+                  Text(
+                    '12/20 - 124',
+                    style: AppStyles.styleRegular16(context)
+                        .copyWith(color: Colors.white),
+                  ),
+                ],
               ),
             ),
-            color: const Color(0XFF4EB7F1),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12))),
-        child: ListTile(
-          contentPadding: const EdgeInsets.only(top: 16, left: 30, right: 40),
-          title: Text(
-            'My Name',
-            style:
-                AppStyles.styleRegular16(context).copyWith(color: Colors.white),
-          ),
-          subtitle: Text(
-            'George Soliman',
-            style: AppStyles.styleMedium20(context),
-          ),
-          trailing: SvgPicture.asset(Assets.imagesGallery),
+            const SizedBox(
+              height: 54 - 28,
+            )
+          ],
         ),
       ),
     );
