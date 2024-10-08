1aa7067afb997fcc8768c698e0b71a018aceb373
GeorgeSoliman <georgelove462@gmail.com>
2024-10-08 17:42:39 +0300

    my card section done

--- lib/widget/dots_indicator.dart
+++ lib/widget/dots_indicator.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_bord/widget/custom_dot_indcator.dart';
 
 class DotsIndicator extends StatelessWidget {
-  const DotsIndicator({super.key});
-
+  const DotsIndicator({super.key, required this.currentpageindex});
+  final int currentpageindex;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: List.generate(3, (index) {
-        return const Padding(
+        return Padding(
           padding: EdgeInsets.only(right: 6),
-          child: CustomDotIndcator(isActive: false),
+          child: CustomDotIndcator(isActive: index == currentpageindex),
         );
       }),
     );
---
+++ lib/widget/my_cards_section.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:responsive_dash_bord/utils/app_style.dart';
+import 'package:responsive_dash_bord/widget/dots_indicator.dart';
+import 'package:responsive_dash_bord/widget/my_cards_page_view.dart';
+
+class MyCardsSection extends StatefulWidget {
+  const MyCardsSection({super.key});
+
+  @override
+  State<MyCardsSection> createState() => _MyCardsSectionState();
+}
+
+class _MyCardsSectionState extends State<MyCardsSection> {
+  late PageController pageController;
+  int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      currentPageIndex = pageController.page!.round();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'My card',
+          style: AppStyles.styleSemiBold20(context),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        MyCardsPageView(
+          pageController: pageController,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        DotsIndicator(
+          currentpageindex: currentPageIndex,
+        )
+      ],
+    );
+  }
+}
